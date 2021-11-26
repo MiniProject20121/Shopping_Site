44f2cfac8d900f7fddd8d380fb6876bfe00fbb06
swapnil2000 <swapnilsharma0220@gmail.com>
2021-11-27 05:08:41 +0530

    add to cart but not linked

--- shoppingcart/addtocart.php
+++ shoppingcart/addtocart.php
@@ -1,17 +1,59 @@
-
+<?php
+session_start();
+require_once("config.php");
+if(!empty($_GET["action"])) {
+switch($_GET["action"]) {
+	case "add":
+		if(!empty($_POST["quantity"])) {
+			$pid=$_GET["pid"];
+			$result=mysqli_query($con,"SELECT * FROM tbl_product WHERE produc='$pid'");
+	          while($productByCode=mysqli_fetch_array($result)){
+			$itemArray = array($productByCode["code"]=>array('name'=>$productByCode["name"], 'code'=>$productByCode["code"], 'quantity'=>$_POST["quantity"], 'price'=>$productByCode["price"], 'image'=>$productByCode["image"]));
+			if(!empty($_SESSION["cart_item"])) {
+				if(in_array($productByCode["code"],array_keys($_SESSION["cart_item"]))) {
+					foreach($_SESSION["cart_item"] as $k => $v) {
+							if($productByCode["code"] == $k) {
+								if(empty($_SESSION["cart_item"][$k]["quantity"])) {
+									$_SESSION["cart_item"][$k]["quantity"] = 0;
+								}
+								$_SESSION["cart_item"][$k]["quantity"] += $_POST["quantity"];
+							}
+					}
+				} else {
+					$_SESSION["cart_item"] = array_merge($_SESSION["cart_item"],$itemArray);
+				}
+			}  else {
+				$_SESSION["cart_item"] = $itemArray;
+			}
+		}
+	}
+	break;
+	case "remove":
+		if(!empty($_SESSION["cart_item"])) {
+			foreach($_SESSION["cart_item"] as $k => $v) {
+					if($_GET["code"] == $k)
+						unset($_SESSION["cart_item"][$k]);				
+					if(empty($_SESSION["cart_item"]))
+						unset($_SESSION["cart_item"]);
+			}
+		}
+	break;
+	case "empty":
+		unset($_SESSION["cart_item"]);
+	break;	
+}
+}
+?>
 <HTML>
 <HEAD>
 <TITLE>Simple PHP Shopping Cart</TITLE>
 <link href="style.css" type="text/css" rel="stylesheet" />
 </HEAD>
 <BODY>
-
-
-<!-- Cart ---->
 <div id="shopping-cart">
 <div class="txt-heading">Shopping Cart</div>
 
-<a id="btnEmpty" href="index.php?action=empty">Empty Cart</a>
+<a id="btnEmpty" href="addtocart.php?action=empty">Empty Cart</a>
 <?php
 if(isset($_SESSION["cart_item"])){
     $total_quantity = 0;
@@ -37,7 +79,7 @@ if(isset($_SESSION["cart_item"])){
 				<td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
 				<td  style="text-align:right;"><?php echo "$ ".$item["price"]; ?></td>
 				<td  style="text-align:right;"><?php echo "$ ". number_format($item_price,2); ?></td>
-				<td style="text-align:center;"><a href="index.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
+				<td style="text-align:center;"><a href="addtocart.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
 				</tr>
 				<?php
 				$total_quantity += $item["quantity"];
@@ -65,7 +107,7 @@ if(isset($_SESSION["cart_item"])){
 
 
 
-<div id="product-grid">
+<!-- <div id="product-grid">
 	<div class="txt-heading">Products</div>
 	<?php
 	$product= mysqli_query($con,"SELECT * FROM tblproduct ORDER BY id ASC");
@@ -74,7 +116,7 @@ if(isset($_SESSION["cart_item"])){
 		
 	?>
 		<div class="product-item">
-			<form method="post" action="index.php?action=add&pid=<?php echo $row["id"]; ?>">
+			<form method="post" action="addtocart.php?action=add&pid=<?php echo $row["id"]; ?>">
 			<div class="product-image"><img src="<?php echo $row["image"]; ?>"></div>
 			<div class="product-tile-footer">
 			<div class="product-title"><?php echo $row["name"]; ?></div>
@@ -90,7 +132,7 @@ if(isset($_SESSION["cart_item"])){
 
 	}
 	?>
-</div>
+</div> -->
 
 
 
