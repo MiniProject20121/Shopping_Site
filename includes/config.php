9141a966f75271c134e16ea4187950896e43bcea
shivansh-mishraa <shivanshmishraskm2@gmail.com>
2021-12-13 01:37:29 +0530

    comment changes

--- includes/config.php
+++ includes/config.php
@@ -6,9 +6,9 @@ define('DB_PASS' ,'');
 define('DB_NAME', 'shopping');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 
-define('DB_PASS' ,'');
-define('DB_NAME', 'shopping');
-$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+// define('DB_PASS' ,'');
+// define('DB_NAME', 'shopping');
+// $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 
 if (mysqli_connect_errno())
 {
