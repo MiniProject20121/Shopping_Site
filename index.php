ad011347e3e7cbdbe298ced7daf828a6573a0241
swapnil2000 <swapnilsharma0220@gmail.com>
2021-12-12 23:47:22 +0530

    latest

--- index.php
+++ index.php
@@ -20,40 +20,10 @@ if(isset($_GET['action']) && $_GET['action']=="add"){
 
     <title>TechMart-Shopping</title>
     <link rel="stylesheet" href="homeitt.css">
-     <!-- Bootstrap Core CSS -->
-     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-	    
-	    <!-- Customizable CSS -->
-	    <link rel="stylesheet" href="assets/css/main.css">
-	    <link rel="stylesheet" href="assets/css/green.css">
-	    <link rel="stylesheet" href="assets/css/owl.carousel.css">
-		<link rel="stylesheet" href="assets/css/owl.transitions.css">
-		<!--<link rel="stylesheet" href="assets/css/owl.theme.css">-->
-		<link href="assets/css/lightbox.css" rel="stylesheet">
-		<link rel="stylesheet" href="assets/css/animate.min.css">
-		<link rel="stylesheet" href="assets/css/rateit.css">
-		<link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
-
-		<!-- Demo Purpose Only. Should be removed in production -->
-		<link rel="stylesheet" href="assets/css/config.css">
-
-		<link href="assets/css/green.css" rel="alternate stylesheet" title="Green color">
-		<link href="assets/css/blue.css" rel="alternate stylesheet" title="Blue color">
-		<link href="assets/css/red.css" rel="alternate stylesheet" title="Red color">
-		<link href="assets/css/orange.css" rel="alternate stylesheet" title="Orange color">
-		<link href="assets/css/dark-green.css" rel="alternate stylesheet" title="Darkgreen color">
-		<!-- Demo Purpose Only. Should be removed in production : END -->
-
-		
-		<!-- Icons/Glyphs -->
-		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
-
-        <!-- Fonts --> 
-		<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
-		
-		<!-- Favicon -->
-		<link rel="shortcut icon" href="assets/images/favicon.ico">
-
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">		
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+	
     <script>
         function stickyMenu(){
             var sticky = document.getElementById('Sticky');
@@ -67,7 +37,11 @@ if(isset($_GET['action']) && $_GET['action']=="add"){
         stickyMenu();
     }
     </script>
-
+<style>
+.a-menu{
+    color:black;
+}
+</style>
 </head>
 <body>
 
@@ -102,9 +76,11 @@ while($row=mysqli_fetch_array($sql))
     <?php $sql=mysqli_query($con,"select id,categoryName  from category limit 6");
 while($row=mysqli_fetch_array($sql))
 {
+    
     ?>
         <div class="categories">
-            <img  src="admin/productimages/<?php echo htmlentities($row['id']);?>/<?php echo htmlentities($row['productImage1']);?>" data-echo="admin/productimages/<?php echo htmlentities($row['productImage1']);?>"  width="180" height="300" alt=""></a><br>
+            
+            <img  src="admin/productimages/categories/<?php $img=2;$img++; echo $img;?>.jpg" data-echo="admin/productimages/<?php echo htmlentities($row['productImage1']);?>"  width="180" height="300" alt=""><a/><br>
             <a class="a-menu" href="category.php?cid=<?php echo $row['id'];?>"> <?php echo $row['categoryName'];?>
         </div>
         <?php } ?>
