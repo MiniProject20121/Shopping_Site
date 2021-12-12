35420b405ebf33a51a06bed06a214b31834344d3
Adarsh Trivedi <adarsh.trivedi_cs19@gla.ac.in>
2021-12-13 00:36:21 +0530

    changes

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER','localhost');
 define('DB_USER','root');
 
-define('DB_PASS' ,'1234');
+define('DB_PASS' ,'');
 define('DB_NAME', 'shopping');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 
